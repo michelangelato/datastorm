e7315e6d301cc220e101934eb9d201b0a7d887ef
Michelangelo Marani <michelangelomarani88@gmail.com>
2016-10-29 14:47:47 +0200

    added /Immobili route with 'inserisci immobile' e 'lista immobili' stub

---
+++ src/webapp/DataStorm.Web/Controllers/ImmobiliController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DataStorm.Web.Controllers
+{
+    public class ImmobiliController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
