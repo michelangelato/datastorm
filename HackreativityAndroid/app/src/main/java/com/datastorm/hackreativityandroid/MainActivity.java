c1c2738e10218cb78cc6bc5e9c6cce0e8b240e0e
Dadino <dadino88>
2016-10-29 12:26:16 +0200

    - initial code

---
+++ HackreativityAndroid/app/src/main/java/com/datastorm/hackreativityandroid/MainActivity.java
@@ -0,0 +1,14 @@
+package com.datastorm.hackreativityandroid;
+
+import android.os.Bundle;
+
+import com.dadino.quickstart.core.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
