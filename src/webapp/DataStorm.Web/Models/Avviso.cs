c50bee0f13c4f0011416477d9d6860cab568edab
Marco Galeassi <marco.galeassi@hotmail.it>
2016-10-29 21:37:46 +0200

    Sistemati mapping tipoimmobile Aggiunta paginazione aziende

--- src/webapp/DataStorm.Web/Models/Avviso.cs
+++ src/webapp/DataStorm.Web/Models/Avviso.cs
@@ -14,21 +14,6 @@ namespace DataStorm.Web.Models
         public virtual List<ImmagineAvviso> ImmaginiAvviso { get; set; }
         public virtual List<LinkAvviso> Links { get; set; }
         public virtual List<AreaMappa> AreeMappe { get; set; }
-
-        public dynamic ToDTO()
-        {
-            return new
-            {
-                Titolo,
-                Descrizione,
-                Immagini = ImmaginiAvviso.Select(i => new { i.Larghezza, i.Altezza, i.TitoloImmagine, i.UrlImmagine }),
-                Links = Links.Select(l => new { l.Titolo, l.Url }),
-                AreeMappe = AreeMappe.Select(am => new
-                {
-                    am.TipoMappa,
-                    PuntiMappa = am.PuntiMappa.Select(p => new { p.LatitudinePunto, p.LongitudinePunto })
-                })
-            };
-        }
+        
     }
 }
