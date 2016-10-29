f101dcd68e9deba2362c95a3090bbe88b73986cc
Marco Galeassi <marco.galeassi@hotmail.it>
2016-10-29 22:10:29 +0200

    Api per gli avvisi, conversioni DTO per avvisi e sottoclassi di avvisi

---
+++ src/webapp/DataStorm.Web/Models/DTO/ImmagineAvvisoDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataStorm.Web.Models.DTO
+{
+    public class ImmagineAvvisoDTO
+    {
+        public virtual string UrlImmagine { get; set; }
+        public virtual int Altezza { get; set; }
+        public virtual int Larghezza { get; set; }
+        public virtual string TitoloImmagine { get; set; }
+        public virtual int Id { get; set; }
+    }
+}
