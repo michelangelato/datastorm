2a960ccf8a833b028ba258f9fd6dd1d38a509a61
Alessio Gardoni <alexglabs@gmail.com>
2016-10-30 02:50:46 +0200

    Messaggi di successo/errore

--- src/webapp/DataStorm.Web/wwwroot/_references.js
+++ src/webapp/DataStorm.Web/wwwroot/_references.js
@@ -16,6 +16,7 @@
 /// <reference path="js/respond.min.js" />
 /// <reference path="js/site.js" />
 /// <reference path="js/spin.min.js" />
+/// <reference path="js/sweetalert2.min.js" />
 /// <reference path="js/wow.min.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
