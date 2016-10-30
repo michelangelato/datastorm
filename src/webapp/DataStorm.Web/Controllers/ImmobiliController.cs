b2108864da9a633543871345d9153a0a3279efe6
Moreno <brightsoul@tiscali.it>
2016-10-30 07:51:51 +0100

    richiesta al servizio di machine learning

--- src/webapp/DataStorm.Web/Controllers/ImmobiliController.cs
+++ src/webapp/DataStorm.Web/Controllers/ImmobiliController.cs
@@ -10,6 +10,8 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using AutoMapper;
 using DataStorm.Web.Models.DTO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace DataStorm.Web.Controllers
 {
@@ -60,6 +62,61 @@ namespace DataStorm.Web.Controllers
                 return PartialView("_DettaglioImmobile", result);
             }
         }
+
+        [HttpPost]
+        public async Task<IActionResult> RichiediValutazione(string numeroDiPiani, string proprieta, string annoDiCostruzione, string costruzione, string percentualeUtilizzo, string uso, string posizione, string cateneCordoli, string comune) {
+            await InvokeRequestResponseService(numeroDiPiani, proprieta, annoDiCostruzione, costruzione, percentualeUtilizzo, uso, posizione, cateneCordoli, comune);
+        }
+
+
+        static async Task<string> InvokeRequestResponseService(string numeroDiPiani, string proprieta, string annoDiCostruzione, string costruzione, string percentualeUtilizzo, string uso, string posizione, string cateneCordoli, string comune)
+        {
+            using (var client = new HttpClient())
+            {
+                var scoreRequest = new
+                {
+
+                    Inputs = new Dictionary<string, StringTable> () { 
+                        { 
+                            "input1", 
+                            new StringTable() 
+                            {
+                                ColumnNames = new string[] {"NumeroDiPiani", "Proprieta", "AnnoDiCostruzione", "Costruzione", "PercentualeUtilizzo", "Uso", "Posizione", "CateneCordoli", "Agibilita", "Comune"},
+                                Values = new string[,] { { numeroDiPiani, proprieta, annoDiCostruzione, costruzione, percentualeUtilizzo, uso, posizione, cateneCordoli, null, comune },  }
+                            }
+                        },
+                    },
+                    GlobalParameters = new Dictionary<string, string>() {
+}
+                };
+                const string apiKey = "vWUGp6GZ80V4VeMoY/j1DXYpiqRB6GUZkS+mTLkAOX8ma+S8UVCSv9/7iHXkbggq+YB6ee8vEBNvLQL3P67naA=="; // Replace this with the API key for the web service
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue( "Bearer", apiKey);
+
+                client.BaseAddress = new Uri("https://ussouthcentral.services.azureml.net/workspaces/9da97d27abde423ab8df23c86c5ce635/services/0f6a6bf2c33f4471991d6c203d2b9398/execute?api-version=2.0&details=true");
+                
+                // WARNING: The 'await' statement below can result in a deadlock if you are calling this code from the UI thread of an ASP.Net application.
+                // One way to address this would be to call ConfigureAwait(false) so that the execution does not attempt to resume on the original context.
+                // For instance, replace code such as:
+                //      result = await DoSomeTask()
+                // with the following:
+                //      result = await DoSomeTask().ConfigureAwait(false)
+
+
+                HttpResponseMessage response = await client.PostAsJsonAsync("", scoreRequest);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception("Richiesta non valida");
+                }
+            }
+        }
+
+
         
     }
 }
