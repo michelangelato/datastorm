c699f1b2ed380f226c3caf797304fbd3529c97dc
Alessio Gardoni <alexglabs@gmail.com>
2016-10-30 04:57:54 +0100

    Seed dei comuni italiani

---
+++ src/webapp/DataStorm.Web/Migrations/20161030035437_FixComuni.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataStorm.Web.Migrations
+{
+    public partial class FixComuni : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Regioni",
+                nullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Denominazione",
+                table: "Province",
+                nullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Sigla",
+                table: "Province",
+                nullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Denominazione",
+                table: "Comuni",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Regioni",
+                maxLength: 40,
+                nullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Denominazione",
+                table: "Province",
+                maxLength: 40,
+                nullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Sigla",
+                table: "Province",
+                type: "char",
+                maxLength: 2,
+                nullable: false);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Denominazione",
+                table: "Comuni",
+                maxLength: 40,
+                nullable: false);
+        }
+    }
+}
